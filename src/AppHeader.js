67cbc9bef94419fe070f93d1f7be2b0e9c25afe6
Александр Бобров <alexandr-alexandrovich2013@yandex.ru>
2019-07-07 01:57:51 +0700

    Ну что, народ, погнали нахуй! Ебаный в рот!

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './AppHeader.css';
+import logo from "./logo.png";
 
 class AppHeader extends Component {
   render() {
     return (
       <div className="AppHeader">
-        <div className="Logo">Кан<b>дед</b>ат</div>
+        <div className="Logo"><img src={logo} width="100px" height="100px" /> <div className="Ded">Кан<b>дед</b>ат</div>
+        <div className="Enter"><a href="#">Войти</a></div>  </div>
       </div>
     );
   }
